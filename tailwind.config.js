3dd1581df8035f09ee8beb5cdfed6692ace09227
Madleyym <tryandaasu@gmail.com>
2025-03-01 23:35:57 +0700

    Update Tailwind CSS configuration for dark mode support and adjust screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      screens: {
-        xs: "360px", // Extra small screen breakpoint
-      },
-      spacing: {
-        0.5: "0.125rem", // Extra small spacing
-      },
       fontSize: {
-        "2xs": "0.65rem", // Extra small font size
+        "2xs": "0.625rem",
+      },
+      screens: {
+        xs: "425px",
       },
     },
   },
