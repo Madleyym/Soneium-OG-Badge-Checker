7e7fe3d952368e4b1ae7c8f2b2ad8773f05a22ba
Madleyym <tryandaasu@gmail.com>
2025-03-05 07:37:06 +0700

    Add PageBottom component to include FAQ and Footer sections

---
+++ app/components/PageBottom.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FAQ from "@/app/components/sections/FAQ";
+import Footer from "@/app/components/sections/UI/Footer";
+const PageBottom: React.FC = () => {
+  return (
+    <>
+      <FAQ />
+      <Footer />
+    </>
+  );
+};
+
+export default PageBottom;
